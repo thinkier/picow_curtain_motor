fff26ca7685e7dd617a4394067d02680fa69d63a
thinkier <26728734+thinkier@users.noreply.github.com>
2022-10-26 18:29:51 +1100

    minor bugfixes

--- firmware/main.py
+++ firmware/main.py
@@ -45,6 +45,7 @@ def push_poll(state: State):
     try:
         payload = {'current_pct': state.get_cur_percentage(), 'target_pct': state.get_tgt_percentage()}
         res = requests.post('http://192.168.1.69:44444/' + NAME, json = payload)
+        print("HomeBridge Appliance Said: " + res.text)
         rej = res.json()
         state.set_percentage(rej['target_pct'])
         time.sleep_ms(1000)
@@ -58,10 +59,10 @@ class State:
     max_steps = round(STEPS_PER_MM * BLINDS_HEIGHT)
     reverse_dir = REVERSE_DIR
     full_step_delay_ms = 1000 / PHASE_PER_SECOND
-    half_step_delay_ms = full_step_delay_ms / 2
+    half_step_delay_us = int(1000 * full_step_delay_ms / 2)
     steps = 0
     target_steps = 0
-    
+
     def __init__(self):
         self.pin_stp = Pin(PIN_STP, Pin.OUT)
         self.pin_dir = Pin(PIN_DIR, Pin.OUT)
@@ -69,25 +70,25 @@ class State:
         self.pin_end = Pin(PIN_END, Pin.IN, Pin.PULL_UP)
         self.timer = Timer()
         self.timer.init(period=int(self.full_step_delay_ms), mode=Timer.PERIODIC, callback=self.move_task)
-    
+
     def __del__(self):
         self.timer.deinit()
-    
+
     def set_percentage(self, pct = 0):
         pct = max(min(pct, 100), 0)
         self.target_steps = round(pct * self.max_steps / 100)
-        
+
     def get_cur_percentage(self):
         return round(100 * self.steps / self.max_steps)
-      
+
     def get_tgt_percentage(self):
         return round(100 * self.target_steps / self.max_steps)
-      
+
     def move_task(self, timer: Timer):
         if not self.pin_end.value():
             # Reset step count when an endstop is reached
             self.steps = self.target_steps
-        
+
         if self.steps > self.target_steps:
             self.steps -= 1
             self.pin_dir.value(self.reverse_dir)
@@ -100,26 +101,25 @@ class State:
             return
         # Engage Stepper
         self.pin_ena.value(0)
-        
+
         # Send a fairly accurately-timed square phase
         self.pin_stp.on()
-        time.sleep_ms(half_step_delay_ms)
+        time.sleep_us(self.half_step_delay_us)
         self.pin_stp.off()
 
 state = State()
 
 while True:
     wlan = wifi_connect()
-    
+
     consec_fail = 0
     while True:
         if not push_poll(state):
             consec_fail += 1
         else:
             consec_fail = 0
-            
+
         if consec_fail > 10:
             print('Reinitializing WiFi...')
             wlan.active(False)
             break
-
