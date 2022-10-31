d47fd5b9356ed25095ec3823c6e4cb3c7d4a09a2
thinkier <26728734+thinkier@users.noreply.github.com>
2022-10-31 16:08:13 +1100

    added UDP support

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ import {
     Service
 } from "homebridge";
 import {Config} from "./config";
-import http, {RequestListener} from "http";
+import http, {RequestListener} from "node:http";
+import dgram from "node:dgram";
+import {RemoteInfo} from "dgram";
 
 let hap: HAP;
 
@@ -27,7 +29,8 @@ class WindowDressingPlugin implements AccessoryPlugin {
     private readonly informationService: Service;
 
     private readonly service: Service;
-    private server;
+    private http_server: http.Server;
+    private udp_server: dgram.Socket;
     private current_pct: number = 0;
     private target_pct: number = -1;
     private last_check: number = 0;
@@ -35,8 +38,10 @@ class WindowDressingPlugin implements AccessoryPlugin {
     constructor(private readonly log: Logging, private readonly config: Config, api: API) {
         this.name = config.name;
         this.config.port ??= 44444;
-        this.server = http.createServer(this.httpListener);
-        this.server.listen(this.config.port);
+        this.http_server = http.createServer(this.httpListener);
+        this.http_server.listen(this.config.port);
+        this.udp_server = dgram.createSocket({type: "udp4"}, this.udpListener);
+        this.udp_server.bind(this.config.port);
 
         this.informationService = new hap.Service.AccessoryInformation()
             .setCharacteristic(hap.Characteristic.Manufacturer, "ACME Pty Ltd");
@@ -85,6 +90,9 @@ class WindowDressingPlugin implements AccessoryPlugin {
     }
 
     httpListener: RequestListener = (req, res) => {
+        const succ = (data: object) => res.writeHead(200, {"content-type": "application/json"}).end(JSON.stringify(data))
+        const err = (error: string) => res.writeHead(400, {"content-type": "application/json"}).end(JSON.stringify({error}))
+
         if (req.method === "POST" && req.headers["content-type"] === "application/json") {
             req.setEncoding("utf8");
             let body = "";
@@ -94,43 +102,49 @@ class WindowDressingPlugin implements AccessoryPlugin {
                     body += read;
                 }
             })
-                .on("end", () => {
-                    let data;
-                    try {
-                        data = JSON.parse(body);
-                    } catch (ex) {
-                        console.error(body, "\n\nRequest Parse Error:", ex);
-                        res.writeHead(400).end("Bad Request: Bad JSON");
-                        return;
-                    }
-
-                    let current_pct = Number.parseInt(data["current_pct"]);
-                    if (current_pct < 0 || current_pct > 100) {
-                        res.writeHead(400).end("Bad Request: Bad JSON: current_pct must be 0-100 inclusive");
-                        return;
-                    }
-                    this.current_pct = current_pct;
-                    this.last_check = Date.now();
-
-                    // Write current to target
-                    if (this.target_pct === -1) {
-                        let target_pct = Number.parseInt(data["target_pct"]);
-                        if (target_pct < 0 || target_pct > 100) {
-                            res.writeHead(400).end("Bad Request: Bad JSON: target_pct must be 0-100 inclusive");
-                            return;
-                        }
-                        this.target_pct = target_pct;
-                    }
-
-                    res.writeHead(200, {
-                        "content-type": "application/json"
-                    }).end(JSON.stringify({"target_pct": this.target_pct}));
-                });
+                .on("end", () => this.genericListener(body, succ, err));
         } else {
-            res.writeHead(400).end("Bad Request: Not POST with application/json");
+            err("Bad Request: Not POST with application/json");
         }
     }
 
+    udpListener = (msg: Buffer, rinfo: RemoteInfo) => {
+        const resp = body => this.udp_server.send(JSON.stringify(body), rinfo.port, rinfo.address);
+        const err = error => resp({error});
+
+        this.genericListener(msg.toString("utf8"), resp, err);
+    }
+
+    genericListener = (payload: string, succ: (data: object) => void, err: (error: string) => void) => {
+        let data: Record<string, number>;
+        try {
+            data = JSON.parse(payload);
+        } catch (ex) {
+            console.error(payload, "\n\nRequest Parse Error:", ex);
+            err("Bad Request: Bad JSON");
+            return;
+        }
+
+        let current_pct = data["current_pct"];
+        if (current_pct < 0 || current_pct > 100) {
+            err("Bad JSON: current_pct must be 0-100 inclusive");
+            return;
+        }
+        this.current_pct = current_pct;
+        this.last_check = Date.now();
+
+        // Write current to target
+        if (this.target_pct === -1) {
+            let target_pct = data["target_pct"];
+            if (target_pct < 0 || target_pct > 100) {
+                err("Bad JSON: target_pct must be 0-100 inclusive");
+                return;
+            }
+            this.target_pct = target_pct;
+        }
+
+        succ({"target_pct": this.target_pct});
+    }
 
     getServices = (): Service[] => [
         this.informationService,
